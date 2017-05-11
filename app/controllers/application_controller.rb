b708de32caf0fc6ce87859d7f4b10ab946e83bd2
Rails Udemi Course <cesarprins@gmail.com>
2017-05-11 04:53:56 +0000

    Se uso la gema devise y bootstrap para autenticacion y vistas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # Para que solicite autenticacion de usuario al entrar
+  before_action :authenticate_user!
 end
