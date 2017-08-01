19c7cc9bd0f6630a34e1253f540e901cd6312f0d
Rails Udemi Course <cesarprins@gmail.com>
2017-08-01 19:24:13 +0000

    Se creo opcion para eliminar amigos y listado y mostrar perfil

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
   
   def full_name
     return "#{first_name} #{last_name}".strip if (first_name || last_name)
@@ -23,6 +25,39 @@ class User < ActiveRecord::Base
     stock = Stock.find_by_ticker(ticker_symbol)
     return false unless stock
     user_stocks.where(stock_id: stock.id).exists?
+  end
+  
+  def not_friends_with?(friend_id)
+    friendships.where(friend_id: friend_id).count < 1
+  end
+
+  def except_current_user(users, user_connected)
+    users.reject {|user| user.id == user_connected.id}
+  end
+
+  def self.search(param)
+    return User.none if param.blank?
+    # borra los espacios en blaco de la variable
+    param.strip!
+    param.downcase!
+    # del resultado de los querys, elimina los repetidos
+    (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    where("lower(#{field_name}) like ?", "%#{param}%")
   end
   
 end
